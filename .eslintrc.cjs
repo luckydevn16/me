71ab44b48344cb3b2ac1668f21741c0c5e9317c9
luckydevn16 <luckydevn16@gmail.com>
2023-10-05 11:05:10 +0200

    set up eslint and prettier
    fix eslint issues

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,41 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
   },
-}
+  extends: [
+    "airbnb",
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "eslint:recommended",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+    tsconfigRootDir: "./",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/semi": 0,
+    "@typescript-eslint/quotes": 0,
+    "react/react-in-jsx-scope": 0,
+    "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
+
+  },
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {NextUIProvider} from '@nextui-org/react'
-import App from './App.tsx'
+import { NextUIProvider } from '@nextui-org/react'
+import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
